b70aa93b353616b25cd0ca7e5374390ea2cdc5bd
kallaballa <amir@viel-zu.org>
2013-05-01 15:12:31 +0200

    added license

--- PixelStencil.cpp
+++ PixelStencil.cpp
@@ -1,8 +1,20 @@
 /*
- * PixelStencil.cpp
+ * PixelStencil
+ * Copyright (C) 2013 Amir Hassan <amir@viel-zu.org>
  *
- *  Created on: Apr 30, 2013
- *      Author: elchaschab
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "PixelStencil.hpp"
@@ -25,14 +37,6 @@ PixelPlanes::PixelPlanes(char* filename) : img(filename), width(img.width()), he
 			planes[p].push_back({w,h});
 		}
 	}
-
-	int i = 0;
-
-	for (auto it = planes.begin(); it != planes.end(); ++it) {
-		std::cout << (*it).first << std::endl;
-		i++;
-	}
-	std::cout << planes.size() << std::endl;
 }
 
 PixelPlanes::~PixelPlanes() {
